86a7a84c4152a57964790b3ee3abfd295fc0e695
kentdrick0029 <kentdrickf@gmail.com>
2023-10-10 19:15:34 +0800

    feat: added javascript code into my html

---
+++ script.js
@@ -0,0 +1,21 @@
+let fruit = prompt("Tell a fruit");
+
+switch(fruit){
+case"banana":
+case"Banana":
+console.log("Banana is good");
+
+break;
+case"orange":
+case"Orange":
+console.log("I'm not a fan of orange");
+
+break;
+case"apple":
+case"Apple":
+console.log("How you like them apples?");
+
+break;
+default:
+console.log("I have never heard of that fruit");
+}
